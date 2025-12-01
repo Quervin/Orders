54ff8d7654badf705cdddbe21a17ad3fe7dabbee
querv <quervinhidalgo@gmail.com>
2025-12-01 15:03:24 -0600

    Complete generic controller

---
+++ Orders.Backend/Controllers/CategoriesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Orders.Backend.UnitsOfWork.Interfaces;
+using Orders.Shared.Entites;
+
+namespace Orders.Backend.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class CategoriesController : GenericController<Category>
+{
+    public CategoriesController(IGenericUnitOfWork<Category> unitOfWork) : base(unitOfWork)
+    {
+    }
+}
