e8f1298258b380ba76301e38e263d7c805c81c86
querv <quervinhidalgo@gmail.com>
2025-12-01 12:35:03 -0600

    Add POST and GET for Countries

---
+++ Orders.Backend/Controllers/CountriesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Orders.Backend.Data;
+using Orders.Shared.Entites;
+
+namespace Orders.Backend.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class CountriesController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public CountriesController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAsync()
+    {
+        return Ok(await _context.Countries.ToListAsync());
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PostAsync(Country country)
+    {
+        _context.Countries.Add(country);
+        await _context.SaveChangesAsync();
+        return Ok(country);
+    }
+}
